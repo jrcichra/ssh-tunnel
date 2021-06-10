eaa2ba46b1b00d26e02307b8682c273a0911ed65
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-06-10 01:23:35 -0400

    no strict host checking

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -x
-ssh -p ${SSH_PORT} -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -nNT -L 0.0.0.0:${LOCAL_PORT}:${REMOTE_ADDR}:${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST}
+ssh -p ${SSH_PORT} -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -o StrictHostKeyChecking=no -nNT -L 0.0.0.0:${LOCAL_PORT}:${REMOTE_ADDR}:${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST}
