1f480b1143fcb1d3392dd2f0cd1c44f9b0e05bfa
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-06-10 01:16:20 -0400

    set -x might say what's wrong

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+set -x
 ssh -p ${SSH_PORT} -o ServerAliveInterval=60 -o ExitOnForwardFailure=yes -nNT -L 0.0.0.0:${LOCAL_PORT}:${REMOTE_ADDR}:${REMOTE_PORT} ${REMOTE_USER}@${REMOTE_HOST}
